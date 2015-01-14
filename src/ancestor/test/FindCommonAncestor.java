c9418b89aef6d4b4c1abcd36bc972bbc51836805
Ananda Kumar <sayanand@gmail.com>
2015-01-14 18:03:23 +1100

    Changed for V2.3

--- src/ancestor/test/FindCommonAncestor.java
+++ src/ancestor/test/FindCommonAncestor.java
@@ -3,4 +3,5 @@ package ancestor.test;
 public interface FindCommonAncestor {
 	public String findCommmonAncestor(String[] commitHashes,
 			String[][] parentHashes, String commitHash1, String commitHash2);
+			//Commented...
 }
